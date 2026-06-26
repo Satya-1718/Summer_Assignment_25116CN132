fff5d0c7edb4d5100e1986efcbb3e1e1ffffe841
Satya Tripathi <satyanarayantripathi047@gmail.com>
2026-06-26 17:46:27 +0530

    Day 1 : Sum of first N natural numbers
    Fix variable name 'n' to 'N' in the for loop for correct summation.

---
+++ day1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int N,sum=0;
+printf("Enter a number:");
+scanf("%d", &N);
+for(int i = 1; i<=n; i++) {
+sum+= i;
+}
+printf("Sum of first %d natural number = %d\n",N , sum);
+return 0;
+}
